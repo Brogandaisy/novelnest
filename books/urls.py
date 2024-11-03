9164943a5af0eb7646aca0150623bc36266556ef
Brogan Carpenter <Brogancarpenter@hotmail.co.uk>
2024-11-03 14:58:02 +0000

    Add urls.py to 'books' and include urls for add, edit and delete books

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.BookListView.as_view(), name='book_list'),  # List all books
+    path('add/', views.BookCreateView.as_view(), name='book_add'),  # Add a new book
+    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),  # View book details
+    path('<int:pk>/edit/', views.BookUpdateView.as_view(), name='book_edit'),  # Edit a book
+    path('<int:pk>/delete/', views.BookDeleteView.as_view(), name='book_delete'),  # Delete a book
+]
