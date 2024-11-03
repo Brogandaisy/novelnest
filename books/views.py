9a935ccef16f7f9fc7164450708aa00557084265
Brogan Carpenter <Brogancarpenter@hotmail.co.uk>
2024-11-03 15:03:09 +0000

    Add book list view to views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class BookListView(LoginRequiredMixin, ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Book.objects.all()
+        return Book.objects.filter(added_by=self.request.user)
