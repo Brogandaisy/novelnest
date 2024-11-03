b391ca2fb0fbc0f3527c3420a524467e61e1be2e
Brogan Carpenter <Brogancarpenter@hotmail.co.uk>
2024-11-03 15:10:04 +0000

    Add BookCreateView - allowing user to add a book to views.py

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# Book List - This displays the list of books - User must be logged in 
 class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'books/book_list.html'
@@ -11,6 +12,17 @@ class BookListView(LoginRequiredMixin, ListView):
             return Book.objects.all()
         return Book.objects.filter(added_by=self.request.user)
 
+# Detailed Book View - This shows further details of the book
 class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+
+# Creating a new book to the users list of books - User must be logged in to add a book
+class BookCreateView(LoginRequiredMixin, CreateView):
+    model = Book
+    template_name = 'books/book_form.html'
+    fields = ['title', 'author', 'status']
+
+    def form_valid(self, form):
+        form.instance.added_by = self.request.user  # Connects the user to the book
+        return super().form_valid(form)
