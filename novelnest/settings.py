84fc958c3409930c5bf8163d8afe74a66be5d372
Brogan Carpenter <Brogancarpenter@hotmail.co.uk>
2024-11-03 17:43:39 +0000

    Create template for book_list.html

--- novelnest/settings.py
+++ novelnest/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'novelnest',
         'USER': 'novelnestuser',
         'PASSWORD': 'yourpassword',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '5433',
     }
 }
