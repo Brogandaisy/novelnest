2b63f437dcd95895db3c8184d0572988c966755a
Brogan Carpenter <Brogancarpenter@hotmail.co.uk>
2024-11-03 15:04:18 +0000

    Add detailed book view to views.py

--- books/views.py
+++ books/views.py
@@ -10,3 +10,7 @@ class BookListView(LoginRequiredMixin, ListView):
         if self.request.user.is_superuser:
             return Book.objects.all()
         return Book.objects.filter(added_by=self.request.user)
+
+class BookDetailView(LoginRequiredMixin, DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
