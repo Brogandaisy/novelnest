536d9c9a74384ea82d8030bbc89f1c185a187471
Brogan Carpenter <Brogancarpenter@hotmail.co.uk>
2024-11-23 20:41:48 +0000

    Use Heroku config vars for SECRET_KEY

--- novelnest/settings.py
+++ novelnest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7g2qdw!5n&6vn#a!by)z9eb#d#k*f7#sf3=e7&c*d&b4bjaq^u"
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
