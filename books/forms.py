060cff27a8272659b33d92bdf4603cbb3b631c50
Brogan Carpenter <Brogancarpenter@hotmail.co.uk>
2024-11-24 15:15:38 +0000

    Amend Review Text Box size for mobile

--- books/forms.py
+++ books/forms.py
@@ -8,7 +8,7 @@ class ReviewForm(forms.ModelForm):
         fields = ["content"]
         widgets = {
             "content": forms.Textarea(
-                attrs={"rows": 4, "placeholder": "Write your review here..."}
+                attrs={"rows": 2, "placeholder": "Write your review here..."}
             ),
         }
 
